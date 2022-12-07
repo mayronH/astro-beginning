7d7f32e9a9fb8ac0f41ae267707d53a2a01aac9d
mayronH <38799771+mayronH@users.noreply.github.com>
2022-12-07 11:38:54 -0300

    astro basics

---
+++ src/components/TheCounter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const addCount = () => {
+  count.value += 1
+}
+</script>
+
+<template>
+  <button @click="addCount">Count: {{ count }}</button>
+</template>
+
+<style scoped></style>
---
+++ src/components/TheMessage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<style scoped>
+div {
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background: #eee;
+
+  color: lime;
+}
+</style>
